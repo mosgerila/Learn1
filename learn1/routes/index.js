a8dd7f216846b88d0092df5c82677a177f06e63d
mosgerila <calin.turcanu1@gmail.com>
2017-10-12 17:58:27 +0300

    BasicAuth

--- learn1/routes/index.js
+++ learn1/routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+  if (!authHeader){
+    res.render('home', { title: 'Express' });  
+  }
+  else{
+  var auth = new Buffer(authHeader.split(' ')[1],
+  'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  console.log('auth:',auth)
   res.render('home', { title: 'Express' });
+  }
 });
 
 module.exports = router;
